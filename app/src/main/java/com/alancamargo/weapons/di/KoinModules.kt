824ad93223f20b41fede1a70b95fe9deebb9885b
Alan Camargo <alan.uk.br@gmail.com>
2020-06-20 19:25:51 +0100

    Add DAOs to dependency injection

--- app/src/main/java/com/alancamargo/weapons/di/KoinModules.kt
+++ app/src/main/java/com/alancamargo/weapons/di/KoinModules.kt
@@ -4,15 +4,14 @@ import com.alancamargo.weapons.data.local.WeaponLocalDataSource
 import com.alancamargo.weapons.data.repository.WeaponRepository
 import com.alancamargo.weapons.data.repository.WeaponRepositoryImpl
 import com.alancamargo.weapons.domain.Weapon
+import com.alancamargo.weapons.framework.db.provider.DatabaseProvider
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 fun getModules() = listOf(data, framework)
 
 private val data = module {
     factory<WeaponRepository> { WeaponRepositoryImpl(get()) }
-}
-
-private val framework = module {
     factory<WeaponLocalDataSource> {
         // TODO
         object : WeaponLocalDataSource {
@@ -21,4 +20,11 @@ private val framework = module {
             }
         }
     }
+}
+
+private val framework = module {
+    factory { DatabaseProvider.getInstance(androidContext()).provideWeaponDao() }
+    factory { DatabaseProvider.getInstance(androidContext()).provideWeaponTypeDao() }
+    factory { DatabaseProvider.getInstance(androidContext()).provideCountryDao() }
+    factory { DatabaseProvider.getInstance(androidContext()).provideCalibreDao() }
 }
