4da857732809f268824a51d4bb6346ac7d39bcc3
Alan Camargo <alan.uk.br@gmail.com>
2020-06-24 15:17:43 +0100

    Add new repositories and data sources to DI

--- app/src/main/java/com/alancamargo/weapons/di/KoinModules.kt
+++ app/src/main/java/com/alancamargo/weapons/di/KoinModules.kt
@@ -2,25 +2,38 @@ package com.alancamargo.weapons.di
 
 import com.alancamargo.weapons.data.crash.CrashReportHelper
 import com.alancamargo.weapons.data.io.IoHelper
-import com.alancamargo.weapons.data.local.WeaponLocalDataSource
+import com.alancamargo.weapons.data.local.*
+import com.alancamargo.weapons.data.repository.calibre.CalibreRepository
+import com.alancamargo.weapons.data.repository.calibre.CalibreRepositoryImpl
+import com.alancamargo.weapons.data.repository.country.CountryRepository
+import com.alancamargo.weapons.data.repository.country.CountryRepositoryImpl
+import com.alancamargo.weapons.data.repository.manufacturer.ManufacturerRepository
+import com.alancamargo.weapons.data.repository.manufacturer.ManufacturerRepositoryImpl
+import com.alancamargo.weapons.data.repository.type.WeaponTypeRepository
+import com.alancamargo.weapons.data.repository.type.WeaponTypeRepositoryImpl
 import com.alancamargo.weapons.data.repository.weapon.WeaponRepository
 import com.alancamargo.weapons.data.repository.weapon.WeaponRepositoryImpl
 import com.alancamargo.weapons.framework.crash.CrashReportHelperImpl
 import com.alancamargo.weapons.framework.db.provider.DatabaseProvider
-import com.alancamargo.weapons.framework.local.WeaponLocalDataSourceImpl
+import com.alancamargo.weapons.framework.local.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 fun getModules() = listOf(data, framework)
 
 private val data = module {
-    factory<WeaponRepository> {
-        WeaponRepositoryImpl(
-            get(),
-            get()
-        )
-    }
+    factory<WeaponRepository> { WeaponRepositoryImpl(get(), get()) }
+    factory<WeaponTypeRepository> { WeaponTypeRepositoryImpl(get(), get()) }
+    factory<CountryRepository> { CountryRepositoryImpl(get(), get()) }
+    factory<CalibreRepository> { CalibreRepositoryImpl(get(), get()) }
+    factory<ManufacturerRepository> { ManufacturerRepositoryImpl(get(), get()) }
+
     factory<WeaponLocalDataSource> { WeaponLocalDataSourceImpl(get(), get(), get(), get(), get()) }
+    factory<WeaponTypeLocalDataSource> { WeaponTypeLocalDataSourceImpl(get()) }
+    factory<CountryLocalDataSource> { CountryLocalDataSourceImpl(get()) }
+    factory<CalibreLocalDataSource> { CalibreLocalDataSourceImpl(get()) }
+    factory<ManufacturerLocalDataSource> { ManufacturerLocalDataSourceImpl(get()) }
+
     factory { IoHelper(get()) }
     factory<CrashReportHelper> { CrashReportHelperImpl() }
 }
