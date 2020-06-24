713acc4a911e778f899ef323c231acfb7c3ad393
Alan Camargo <alan.uk.br@gmail.com>
2020-06-24 14:44:44 +0100

    Implement WeaponTypeLocalDataSource

---
+++ app/src/main/java/com/alancamargo/weapons/framework/local/WeaponTypeLocalDataSourceImpl.kt
@@ -0,0 +1,16 @@
+package com.alancamargo.weapons.framework.local
+
+import com.alancamargo.weapons.data.local.WeaponTypeLocalDataSource
+import com.alancamargo.weapons.domain.WeaponType
+import com.alancamargo.weapons.framework.db.WeaponTypeDao
+import com.alancamargo.weapons.framework.model.conversions.fromDbToDomain
+
+class WeaponTypeLocalDataSourceImpl(
+    private val weaponTypeDao: WeaponTypeDao
+) : WeaponTypeLocalDataSource {
+
+    override suspend fun getWeaponTypes(): List<WeaponType> = weaponTypeDao.selectAll().map {
+        it.fromDbToDomain()
+    }
+
+}
---
+++ app/src/test/java/com/alancamargo/weapons/framework/local/WeaponTypeLocalDataSourceImplTest.kt
@@ -0,0 +1,51 @@
+package com.alancamargo.weapons.framework.local
+
+import com.alancamargo.weapons.framework.db.WeaponTypeDao
+import com.alancamargo.weapons.framework.model.entities.DbWeaponType
+import com.google.common.truth.Truth.assertThat
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import java.io.IOException
+
+class WeaponTypeLocalDataSourceImplTest {
+
+    @MockK lateinit var mockWeaponTypeDao: WeaponTypeDao
+
+    private lateinit var localDataSource: WeaponTypeLocalDataSourceImpl
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        localDataSource = WeaponTypeLocalDataSourceImpl(mockWeaponTypeDao)
+    }
+
+    @Test
+    fun shouldGetWeaponTypes() = runBlocking {
+        val data = listOf(
+            DbWeaponType(1L, DbWeaponType.NAME_PISTOL, category = null),
+            DbWeaponType(2L, DbWeaponType.NAME_CARBINE, category = null)
+        )
+        coEvery {
+            mockWeaponTypeDao.selectAll()
+        } returns data
+
+        val weaponTypes = localDataSource.getWeaponTypes()
+
+        assertThat(weaponTypes.size).isEqualTo(2)
+    }
+
+    @Test(expected = IOException::class)
+    fun getWeaponTypes_databaseThrowsException_shouldThrow() {
+        coEvery { mockWeaponTypeDao.selectAll() } throws IOException()
+
+        runBlocking {
+            localDataSource.getWeaponTypes()
+        }
+    }
+
+}
