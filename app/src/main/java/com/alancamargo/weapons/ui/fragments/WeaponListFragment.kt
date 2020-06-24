8540729bd843f3f180cab1900f98ce6abe7ca086
Alan Camargo <alan.uk.br@gmail.com>
2020-06-24 20:47:42 +0100

    Implementing WeaponListFragment layout

--- app/src/main/java/com/alancamargo/weapons/ui/fragments/WeaponListFragment.kt
+++ app/src/main/java/com/alancamargo/weapons/ui/fragments/WeaponListFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.alancamargo.weapons.R
 import com.alancamargo.weapons.domain.Weapon
+import com.alancamargo.weapons.ui.tools.hide
+import com.alancamargo.weapons.ui.tools.show
 import com.alancamargo.weapons.ui.viewmodel.WeaponListViewModel
+import kotlinx.android.synthetic.main.fragment_weapon_list.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class WeaponListFragment : Fragment() {
+class WeaponListFragment : Fragment(R.layout.fragment_weapon_list) {
 
     private val viewModel by viewModel<WeaponListViewModel>()
 
@@ -28,15 +32,19 @@ class WeaponListFragment : Fragment() {
     }
 
     private fun displayWeapons(weapons: List<Weapon>) {
+        groupError.hide()
+        progressBar.hide()
         // TODO
     }
 
     private fun showError() {
-        // TODO
+        progressBar.hide()
+        groupError.show()
     }
 
     private fun showLoading() {
-        // TODO
+        groupError.hide()
+        progressBar.show()
     }
 
 }
---
+++ app/src/main/java/com/alancamargo/weapons/ui/tools/ViewExtensions.kt
@@ -0,0 +1,11 @@
+package com.alancamargo.weapons.ui.tools
+
+import android.view.View
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    visibility = View.GONE
+}
