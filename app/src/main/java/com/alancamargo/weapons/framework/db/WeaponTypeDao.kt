e6ba71f6771587f4503a4248d76b99ea168663fc
Alan Camargo <alan.uk.br@gmail.com>
2020-06-24 16:54:18 +0100

    Remove unused queries

--- app/src/main/java/com/alancamargo/weapons/framework/db/WeaponTypeDao.kt
+++ app/src/main/java/com/alancamargo/weapons/framework/db/WeaponTypeDao.kt
@@ -13,10 +13,4 @@ interface WeaponTypeDao {
     @Query("SELECT * FROM WEAPON_TYPE WHERE ID = :id")
     suspend fun selectById(id: Long): DbWeaponType
 
-    @Query("SELECT * FROM WEAPON_TYPE WHERE NAME_ID = :name")
-    suspend fun selectByNameId(name: String): List<DbWeaponType>
-    
-    @Query("SELECT * FROM WEAPON_TYPE WHERE NAME_ID = :nameId AND CATEGORY_ID = :categoryId")
-    suspend fun selectByNameIdAndCategoryId(nameId: String, categoryId: String): List<DbWeaponType>
-
 }
