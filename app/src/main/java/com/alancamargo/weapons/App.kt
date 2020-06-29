dde05387e5bfeed5e46be2840c2c7f6b6b6417e1
Alan Camargo <alan.uk.br@gmail.com>
2020-06-29 20:33:44 +0100

    Set accent colour

--- app/src/main/java/com/alancamargo/weapons/App.kt
+++ app/src/main/java/com/alancamargo/weapons/App.kt
@@ -1,13 +1,13 @@
 package com.alancamargo.weapons
 
-import androidx.multidex.MultiDexApplication
+import android.app.Application
 import com.alancamargo.weapons.di.getModules
 import com.google.android.gms.ads.MobileAds
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 @Suppress("unused")
-class App : MultiDexApplication() {
+class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
