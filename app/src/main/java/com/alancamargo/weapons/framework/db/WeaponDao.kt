6b97465692f318169df07b7144a43d484fc68091
Alan Camargo <alan.uk.br@gmail.com>
2020-07-01 21:50:30 +0100

    Add leniency to queries by name

--- app/src/main/java/com/alancamargo/weapons/framework/db/WeaponDao.kt
+++ app/src/main/java/com/alancamargo/weapons/framework/db/WeaponDao.kt
@@ -10,7 +10,7 @@ interface WeaponDao {
     @Query("SELECT * FROM WEAPON")
     suspend fun selectAll(): List<DbWeapon>
 
-    @Query("SELECT * FROM WEAPON WHERE NAME = :name")
+    @Query("SELECT * FROM WEAPON WHERE NAME LIKE :name")
     suspend fun selectByName(name: String): List<DbWeapon>
 
     @Query("SELECT * FROM WEAPON WHERE YEAR_ID = :yearId")
