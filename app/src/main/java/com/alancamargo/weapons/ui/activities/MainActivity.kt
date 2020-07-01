242212c5b885f8e6e8b4968d7f93d0e15080d7eb
Alan Camargo <alan.uk.br@gmail.com>
2020-07-01 22:04:43 +0100

    Add menu to MainActivity

--- app/src/main/java/com/alancamargo/weapons/ui/activities/MainActivity.kt
+++ app/src/main/java/com/alancamargo/weapons/ui/activities/MainActivity.kt
@@ -2,6 +2,8 @@ package com.alancamargo.weapons.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.alancamargo.weapons.R
 import com.alancamargo.weapons.ui.adapter.QueryAdapter
@@ -10,6 +12,7 @@ import com.alancamargo.weapons.ui.queries.WeaponQuery
 import com.alancamargo.weapons.ui.queries.WeaponQueryType
 import com.alancamargo.weapons.ui.tools.loadAds
 import com.alancamargo.weapons.ui.viewmodel.QueryViewModel
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -23,6 +26,19 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), QueryAdapter.OnI
         recyclerView.adapter = adapter
         adapter.setData(viewModel.getQueries())
         adView.loadAds()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.itemAbout -> showAppInfo()
+            R.id.itemPrivacyNotice -> showPrivacyNotice()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onItemClick(query: WeaponQueryType) = when (query) {
@@ -68,6 +84,24 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), QueryAdapter.OnI
     private fun openYearListActivity() {
         val intent = Intent(this, YearListActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun showAppInfo() {
+        val appName = getString(R.string.app_name)
+        val appVersion = packageManager.getPackageInfo(packageName, 0).versionName
+        val title = getString(R.string.app_name_format, appName, appVersion)
+
+        MaterialAlertDialogBuilder(this)
+            .setTitle(title)
+            .setMessage(R.string.about_message)
+            .show()
+    }
+
+    private fun showPrivacyNotice() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(R.string.privacy_notice)
+            .setMessage(R.string.privacy_notice_text)
+            .show()
     }
 
 }
