cc08ac187b688f99505ea61d0830938cb5020360
Alan Camargo <alan.uk.br@gmail.com>
2023-07-28 13:24:41 -0300

    Fix extensions

--- core/src/main/java/com/alancamargo/weapons/core/extensions/ViewExtensions.kt
+++ core/src/main/java/com/alancamargo/weapons/core/extensions/ViewExtensions.kt
@@ -1,13 +1,9 @@
 package com.alancamargo.weapons.core.extensions
 
-import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
-import android.widget.ProgressBar
 import androidx.core.view.isVisible
-import coil.ImageLoader
-import coil.request.ImageRequest
 import com.google.android.material.textview.MaterialTextView
 
 fun MaterialTextView.setTextOrHide(text: String?) {
@@ -20,32 +16,6 @@ fun ImageView.setDrawableOrHide(drawable: Drawable?) {
     drawable?.let {
         setImageDrawable(it)
     } ?: hide()
-}
-
-suspend fun ImageView.load(
-    imageLoader: ImageLoader,
-    path: String,
-    progressBar:
-    ProgressBar,
-    errorTextView: MaterialTextView
-) {
-    val inputStream = context.assets.open(path)
-    val bitmap = BitmapFactory.decodeStream(inputStream)
-
-    val request = ImageRequest.Builder(context)
-        .data(bitmap)
-        .target(onStart = {
-            errorTextView.isVisible = false
-            progressBar.isVisible = true
-        }, onError = {
-            progressBar.isVisible = false
-            errorTextView.isVisible = true
-        }, onSuccess = {
-            progressBar.isVisible = false
-            setImageDrawable(it)
-        }).build()
-
-    imageLoader.execute(request)
 }
 
 private fun View.hide() {
